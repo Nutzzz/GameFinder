ee627cb034a70680cd730998845ca6158808d7af
Nutzzz <enussbaum@hotmail.com>
2024-06-27 11:29:57 -0700

    Update Settings.cs
    Rename passed variable names to better match internal variable names.

--- src/GameFinder.Common/Settings.cs
+++ src/GameFinder.Common/Settings.cs
@@ -7,11 +7,11 @@ public class Settings
     public bool OwnedOnly { get; set; } = false;
     public bool GamesOnly { get; set; } = false;
 
-    public Settings(bool installed = false, bool parents = false, bool owned = false, bool games = false)
+    public Settings(bool installedOnly = false, bool baseOnly = false, bool ownedOnly = false, bool gamesOnly = false)
     {
-        InstalledOnly = installed;
-        BaseOnly = parents;
-        OwnedOnly = owned;
-        GamesOnly = games;
+        InstalledOnly = installedOnly;
+        BaseOnly = baseOnly;
+        OwnedOnly = ownedOnly;
+        GamesOnly = gamesOnly;
     }
 }
